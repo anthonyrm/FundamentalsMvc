834873f124229704de97539007a338e4c5901f05
Anthony <anthonyrm8@gmaill.com>
2016-08-24 19:31:40 -0500

    Create List Customer with EF

---
+++ NetFundamentals.Repository/BaseRepository.cs
@@ -0,0 +1,41 @@
+﻿using NetFundamentals.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+namespace NetFundamentals.Repository
+{
+    public class BaseRepository<T> : IRepository<T> where T : class
+    {
+        private ChinookContext context;
+        public BaseRepository()
+        {
+            context = new ChinookContext();
+        }
+        public int Add(T entity)
+        {
+            context.Entry(entity).State = System.Data.Entity.EntityState.Added;
+            return context.SaveChanges();
+        }
+        public int Delete(T entity)
+        {
+            context.Entry(entity).State = System.Data.Entity.EntityState.Deleted;
+            return context.SaveChanges();
+        }
+        public int Update(T entity)
+        {
+            context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            return context.SaveChanges();
+        }
+        public T GetById(Expression<Func<T, bool>> match)
+        {
+            return context.Set<T>().FirstOrDefault(match);
+        }
+        public IEnumerable<T> GetList()
+        {
+            return context.Set<T>();
+        }
+    }
+}
---
+++ NetFundamentals.Repository/ChinookContext.cs
@@ -0,0 +1,25 @@
+﻿using NetFundamentals.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace NetFundamentals.Repository
+{
+    public class ChinookContext : DbContext
+    {
+        public ChinookContext() : base("NetFundamentals")
+        {
+            Database.SetInitializer<ChinookContext>(null);
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
