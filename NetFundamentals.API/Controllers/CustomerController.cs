82e4e4bcbf2193ff8ccf68de55d2bcc1609ec3f2
Anthony <anthonyrm8@gmaill.com>
2016-09-01 19:04:02 -0500

    Add web.api

---
+++ NetFundamentals.API/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using NetFundamentals.Model;
+using NetFundamentals.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NetFundamentals.API.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private IRepository<Customer> repository;
+        public CustomerController() {
+            repository = new BaseRepository<Customer>();
+        }
+
+        [HttpPut]
+        public IHttpActionResult Create(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(repository.Add(customer));
+        }
+
+        [HttpGet]
+        public IHttpActionResult Details(int? id)
+        {
+            var customer = repository.GetById(x => x.CustomerId == id);
+            if (customer == null)
+                return BadRequest(ModelState);
+            return Ok(customer);
+        }
+
+    }
+}
